e2bc1a3e8852d6a414e55c3510330fc897ce6287
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-05 15:21:55 +0900

    Add files via upload

---
+++ 코린이/scripts.js
@@ -0,0 +1,23 @@
+$("#team").click(function() {
+    document.location.href = 'index.html';
+});
+
+$("#members").click(function() {
+    document.location.href = 'members.html';
+});
+
+$("#join").click(function() {
+    document.location.href = '#';
+});
+
+$("#news").click(function() {
+    document.location.href = '#';
+});
+
+$("#update").click(function() {
+    document.location.href = '#';
+});
+
+$("#delete").click(function() {
+    document.location.href = '#';
+});
