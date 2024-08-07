920020baf67799eb372733436f5d29a047be4e7e
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-07 15:39:19 +0900

    url 경로 수정

--- corini/scripts.js
+++ corini/scripts.js
@@ -1,45 +1,47 @@
 $("#team").click(function () {
-    document.location.href = 'index.html';
+    const indexURL = "/";
+    window.history.pushState(null, "", indexURL);
+    window.location.href = 'corini/index.html';
 });
 
 $("#members").click(function () {
-    document.location.href = 'members.html';
+    const membersURL = "/member";
+    window.history.pushState(null, "", membersURL);
+    window.location.href = 'corini/members.html';
 });
 
 $("#join").click(function () {
-    document.location.href = 'register.html';
+    const registerURL = "/join";
+    window.history.pushState(null, "", registerURL);
+    window.location.href = 'corini/register.html';
 });
 
 $("#news").click(function () {
-    document.location.href = 'news_list.html';
+    const news_listURL = "/news";
+    window.history.pushState(null, "", news_listURL);
+    window.location.href = 'corini/news_list.html';
 });
 
 $("#add").click(function () {
-    document.location.href = 'news_view.html';
+    const news_viewURL = "/news";
+    window.history.pushState(null, "", news_viewURL);
+    window.location.href = 'corini/news_view.html';
 });
 
 $("#back").click(function () {
-    document.location.href = 'news_list.html';
+    window.history.back();
 });
 
 $("#back_2").click(function () {
-    document.location.href = 'index.html';
-});
-
-$("#update").click(function () {
-    document.location.href = '#';
-});
-
-$("#delete").click(function () {
-    document.location.href = '#';
+    window.history.back();
 });
 
 function mbtiCheck(e) {
     e.value = e.value.replace(/[^A-Za-z]/ig, '');
 }
 
-function passwordCheck(object){
-    if(object.value.length > object.maxLength){
+function passwordCheck(object) {
+    if (object.value.length > object.maxLength) {
         object.value = object.value.slice(0, object.maxLength);
     }
 }
