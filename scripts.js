8654c1cfae8222e2bd4603fe38eef269b3934c0d
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-08 19:04:14 +0900

    버튼 하이라이트 효과

--- scripts.js
+++ scripts.js
@@ -1,45 +1,48 @@
-$("#team").click(function () {
-    document.location.href = 'index.html';
-});
+$(document).ready(function () {
 
-$("#members").click(function () {
-    document.location.href = 'members.html';
-});
+    $("#team").click(function () {
+        document.location.href = 'index.html';
+    });
 
-$("#join").click(function () {
-    document.location.href = 'register.html';
-});
+    $("#members").click(function () {
+        document.location.href = 'members.html';
+    });
 
-$("#news").click(function () {
-    document.location.href = 'news_list.html';
-});
+    $("#join").click(function () {
+        document.location.href = 'register.html';
+    });
 
-$("#add").click(function () {
-    document.location.href = 'news_view.html';
-});
+    $("#news").click(function () {
+        document.location.href = 'news_list.html';
+    });
 
-$("#back").click(function () {
-    document.location.href = 'members.html';
-});
+    $("#add").click(function () {
+        document.location.href = 'news_view.html';
+    });
 
-$("#back2").click(function () {
-    document.location.href = 'news_list.html';
-});
+    $("#back").click(function () {
+        document.location.href = 'members.html';
+    });
 
-$("#update").click(function () {
-    document.location.href = '#';
-});
+    $("#back2").click(function () {
+        document.location.href = 'news_list.html';
+    });
 
-$("#delete").click(function () {
-    document.location.href = '#';
-});
+    $("#update").click(function () {
+        document.location.href = '#';
+    });
 
-function mbtiCheck(e) {
-    e.value = e.value.replace(/[^A-Za-z]/ig, '');
-}
+    $("#delete").click(function () {
+        document.location.href = '#';
+    });
 
-function passwordCheck(object){
-    if(object.value.length > object.maxLength){
-        object.value = object.value.slice(0, object.maxLength);
+    function mbtiCheck(e) {
+        e.value = e.value.replace(/[^A-Za-z]/ig, '');
     }
-}
+
+    function passwordCheck(object) {
+        if (object.value.length > object.maxLength) {
+            object.value = object.value.slice(0, object.maxLength);
+        }
+    }
+});
